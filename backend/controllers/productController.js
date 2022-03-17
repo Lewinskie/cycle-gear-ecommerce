195a405c4f43bf36fb98a1a8795338be7d8671c2
Lewinskie <wangaralewis@gmail.com>
2022-03-17 09:56:24 +0300

    added pagination, filtering and sorting logic to productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,35 +7,73 @@ class APIfeatures {
     this.queryString = queryString;
   }
   filtering() {
-    const queryObj = { ...this.queryString }; //queryString = req.query
-    console.log(queryObj);
+    const queryObj = { ...this.queryString }; //queryString=req.body
+    console.log({ before: queryObj }); //before delete page
 
     const excludedFields = ["page", "sort", "limit"];
-    excludedFields.forEach((el) => delete queryObj[el]);
+    excludedFields.forEach((item) => delete queryObj[item]);
 
-    console.log(queryObj);
+    console.log({ after: queryObj }); //after delete page
+
+    let queryStr = JSON.stringify(queryObj);
+
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lt|lte|regex)\b/g,
+      (match) => "$" + match
+    );
+    console.log({ queryObj, queryStr });
+    this.query.find(JSON.parse(queryStr));
+
+    // gte = GREATER THAN OR EQUAL TO
+    //lte = LESS THAN OR EQUAL TO
+    // lt = LESS THAN
+    // gt = GREATER THAN
 
     return this;
   }
 
-  sorting() {}
+  sorting() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join("");
+      console.log(sortBy);
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+    return this;
+  }
 
-  paginating() {}
+  paginating() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 9;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
 }
 
 //FETCH PRODUCTS
 const getProducts = async (req, res) => {
   try {
     //QUERIES
-    // console.log(req.query);
-    const features = new APIfeatures(Product.find(), req.query);
-    const product = await features.query;
+    const features = new APIfeatures(Product.find(), req.query)
+      .filtering()
+      .sorting()
+      .paginating();
+    const products = await features.query;
 
-    const products = await Product.find();
-    if (products.length === 0)
-      return res.status(400).json({ msg: "No products found!" });
+    res.json({
+      status: "Success",
+      result: products.length,
+      products: products,
+    });
 
-    return res.status(200).json({ products: products });
+    // const products = await Product.find();
+    // if (products.length === 0)
+    //   return res.status(400).json({ msg: "No products found!" });
+
+    // return res.status(200).json({ products: products });
   } catch (error) {
     return res.status(500).json({ msg: error.message });
   }
